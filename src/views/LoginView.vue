c03210b7307bfe4805165532c57b44a69e14d6a1
Nickjerome <2464489338@qq.com>
2024-03-16 02:43:00 +0800

    Sat Mar 16 02:43:00 CST 2024 inscode

---
+++ src/views/LoginView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { NCard, NTabs, NTabPane, NForm, NFormItemRow, NInput, NButton } from 'naive-ui';
+</script>
+
+<template>
+  <n-card>
+    <n-tabs type="segment" animated>
+      <n-tab-pane name="signin" tab="登录">
+        <n-form>
+          <n-form-item-row label="用户名">
+            <n-input />
+          </n-form-item-row>
+          <n-form-item-row label="密码">
+            <n-input />
+          </n-form-item-row>
+        </n-form>
+        <n-button type="primary" block secondary strong>
+          登录
+        </n-button>
+      </n-tab-pane>
+      <n-tab-pane name="signup" tab="注册">
+        <n-form>
+          <n-form-item-row label="用户名">
+            <n-input />
+          </n-form-item-row>
+          <n-form-item-row label="密码">
+            <n-input />
+          </n-form-item-row>
+          <n-form-item-row label="重复密码">
+            <n-input />
+          </n-form-item-row>
+        </n-form>
+        <n-button type="primary" block secondary strong>
+          注册
+        </n-button>
+      </n-tab-pane>
+    </n-tabs>
+  </n-card>
+</template>
+
+<style scoped>
+
+</style>
