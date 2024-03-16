316c5fc922d1ff495da92097039177b8719e5e80
Nickjerome <2464489338@qq.com>
2024-03-16 17:24:00 +0800

    Sat Mar 16 17:24:00 CST 2024 inscode

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+const subtitle = "微博信息系统";
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/LoginView.vue')
+      component: () => import('../views/LoginView.vue'),
+      meta: {
+        title: "登陆"
+      }
     },
     {
       path: '/hot',
       name: 'hot',
-      component: () => import('../views/PanelView.vue')
+      component: () => import('../views/PanelView.vue'),
+      meta: {
+        title: "热榜"
+      }
     },
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title + " - " + subtitle;
+  }
+  next();
+});
 
 export default router
